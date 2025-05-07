7c28a5f51a0e552422e9fda7883d8ec0a35efb60
Federico Ortiz <sib2000@gmail.com>
2025-05-07 18:54:09 -0300

    restore i think

---
+++ client/src/Components/files.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import * as XLSX from 'xlsx';
+
+function FileInput() {
+    const [data, setData] = React.useState(null);
+
+    const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+            const workbook = XLSX.read(event.target.result, { type: 'binary' });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const sheetData = XLSX.utils.sheet_to_json(sheet);
+
+            setData(sheetData);
+        };
+
+        reader.readAsBinaryString(file);
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileUpload} />
+            {data && (
+                <div>
+                    <h2>Imported Data:</h2>{/* 
+                         <pre>{JSON.stringify(data, null, 2)}</pre>   */}
+                     {data.map((contact,index) => {
+                        if (contact.__EMPTY == "1")
+                        return (
+                            <>{index} - {contact.Contacto} - {contact.servicio} - {contact.Clave}<br/>
+                            </>
+                        )
+                    })}
+                    <p>{(data.length)}</p>
+                </div>
+            )}
+
+        </div>
+    );
+}
+
+export default FileInput;
